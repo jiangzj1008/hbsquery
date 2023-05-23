abca31e47748c260856601bae3f250c86d986d31
jiangzhijian <jiangzhijian@joyy.com>
2023-05-23 14:02:46 +0800

    feat: support select element node

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -7,4 +7,24 @@ describe('basic', () => {
 
     expect($.template()).toBe(code);
   });
+
+  it('load element', () => {
+    const code = '<div class="first"></div><div></div>';
+    const $ = load(code);
+    const $divs = $('div');
+
+    $divs.forEach((el) => {
+      const $el = $(el);
+      if ($el.hasClass('first')) {
+        $el.attr({ 'data-type': 'first' });
+      } else {
+        $el.attr({ 'data-type': 'normal' });
+      }
+    });
+
+    const target =
+      '<div data-type="first" class="first"></div><div data-type="normal"></div>';
+
+    expect($.template()).toBe(target);
+  });
 });
